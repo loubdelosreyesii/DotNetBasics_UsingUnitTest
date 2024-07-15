df192863a4f26c71ab6cf7a13663d317f790d097
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2024-07-16 04:27:54 +0800

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics_UsingUnitTest
+{
+    /// <summary>
+    /// Bank account demo class.
+    /// </summary>
+    public class BankAccount
+    {
+        private readonly string m_customerName;
+        private double m_balance;
+
+        private BankAccount()
+        {
+        }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get
+            {
+                return m_customerName;
+            }
+        }
+
+        public double Balance
+        {
+            get
+            {
+                return m_balance;
+            }
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                Console.WriteLine("Valid Transaction.");
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            // intentionally incorrect code
+            m_balance -= amount; 
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+    }
+}
